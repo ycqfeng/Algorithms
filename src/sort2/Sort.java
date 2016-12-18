444e26da87b1ee22ebde51fb4da0b7dc84fab705
ycqfeng <ycqfeng@vip.qq.com>
2016-12-19 01:07:39 +0800

    添加了两种排序算法 1，插入法排序 2，分治法排序

--- src/sort2/Sort.java
+++ src/sort2/Sort.java
@@ -26,6 +26,13 @@ public class Sort {
         }
     }
 
+    /**
+     * 分治法排序
+     * 效率比插入法高很多
+     * @param list 需要排序的数组，需要实现接口SortInterfaceCompareTo
+     * @param p 开始位置
+     * @param r 结束位置
+     */
     static public void sort_merge(SortInterfaceCompareTo[] list, int p, int r){
         if (p < r){
             int q = (p+r)/2;
@@ -35,35 +42,45 @@ public class Sort {
         }
     }
 
+    /**
+     * 分治法排序合并函数
+     * @param list 需要合并的数组
+     * @param p 开始位置
+     * @param q 中间位置
+     * @param r 结束位置
+     */
     static private void merge(SortInterfaceCompareTo[] list, int p, int q, int r){
         int n1 = q - p + 1;
         int n2 = r - q;
         SortInterfaceCompareTo[] L,R;
-        L = new SortInterfaceCompareTo[n1+1];
-        R = new SortInterfaceCompareTo[n2+1];
+        L = new SortInterfaceCompareTo[n1];
+        R = new SortInterfaceCompareTo[n2];
         for (int i = 0 ; i < n1 ; i++){
             L[i] = list[p+i];
         }
         for (int j = 0 ; j < n2 ; j++){
             R[j] = list[q+j+1];
         }
-        boolean flag;
         int i = 0;
         int j = 0;
         for (int k = p ; k <= r ; k++){
-            if (L[i] == null){
-                flag = !R[j].compareTo(L[i]);
+            if (i == L.length){
+                list[k] = R[j];
+                j += 1;
             }
-            else {
-                flag = L[i].compareTo(R[j]);
-            }
-            if (flag){
+            else if (j == R.length){
                 list[k] = L[i];
                 i += 1;
             }
             else {
-                list[k] = R[j];
-                j += 1;
+                if (L[i].compareTo(R[j])){
+                    list[k] = L[i];
+                    i += 1;
+                }
+                else{
+                    list[k] = R[j];
+                    j += 1;
+                }
             }
         }
 
@@ -80,38 +97,39 @@ public class Sort {
             @Override
             public boolean compareTo(SortInterfaceCompareTo e) {
                 boolean flag;
-                if (e == null){
-                    flag = true;
-                }
-                else {
                     A a = (A) e;
                     if (this.a > a.a) {
                         flag = true;
                     } else {
                         flag = false;
                     }
-                }
                 return flag;
             }
         }
 
+
+
         Random random = new Random();
-        A[] list = new A[10];
-        String str1 = "";
+        A[] list = new A[1000000];
+        //String str1 = "";
         for (int i = 0 ; i < list.length ; i++){
-            list[i] = new A(random.nextInt(1000));
-            str1 += list[i].a + " ";
+            list[i] = new A(random.nextInt(9999999));
+            //str1 += list[i].a + " ";
         }
-        System.out.println(str1);
+        //System.out.println(str1);
+        long start = System.currentTimeMillis();
         //Sort.sort_insertion(list);
         Sort.sort_merge(list, 0, list.length-1);
-        String str2 = "";
+        long end = System.currentTimeMillis();
+        /*String str2 = "";
         for (int i = 0 ; i < list.length ; i++){
             str2 += list[i].a + " ";
         }
-        System.out.println(str2);
+        System.out.println(str2);*/
 
 
+
+        System.out.println("运行时间：" +(end-start)+"ms");
 
     }
 }
